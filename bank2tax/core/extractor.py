c02364d3c80f00851031e7a19aee019ddee573fb
Stefan Waterval <stefanwaterval@gmail.com>
2026-02-21 14:58:42 +0100

    Update prompt to look for account number

--- bank2tax/core/extractor.py
+++ bank2tax/core/extractor.py
@@ -12,6 +12,8 @@ class ExtractorAgent:
         system = (
             "You extract structured data from bank statement text.\n"
             "Statements may be written in German or French.\n"
+            "Account numbers are made of 3 parts separated by '.' or '-'\n"
+            "Keep the account number format EXACTLY AS IS\n"
             "Return ONLY valid JSON that matches the provided JSON Schema.\n"
             "No markdown fences, no commentary, no extra keys.\n"
             "If a value is not present in the document, set it to null.\n"
@@ -20,7 +22,7 @@ class ExtractorAgent:
         user = (
             "Extract all accounts mentioned.\n"
             "For each account, provide:\n"
-            "- iban\n"
+            "- account_number\n"
             "- ending_balance\n"
             "- currency\n"
             "- institution\n\n"
